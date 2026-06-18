38ce72a78f38f3ca2e2843a3344c35ae5bdeaaf9
muhammad023 <abdullaevmuhammad132@gmail.com>
2026-06-18 18:45:39 +0500

    user table

--- apps/models.py
+++ apps/models.py
@@ -2,4 +2,60 @@ from django.db import models
 
 # Create your models here.
 
-# Hello team
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.db.models import CharField, DecimalField, Model, CASCADE, ForeignKey, ImageField, TextField, DateTimeField, \
+    IntegerField, SmallIntegerField, EmailField
+
+
+class CustomUserManager(UserManager):
+
+    def _create_user_object(self, phone, password, **extra_fields):
+        if not phone:
+            raise ValueError("The given phone must be set")
+        phone = self.normalize_email(phone)
+        user = self.model(phone=phone, **extra_fields)
+        user.password = make_password(password)
+        return user
+
+    def _create_user(self, phone, password, **extra_fields):
+        """
+        Create and save a user with the given  phone, and password.
+        """
+        user = self._create_user_object(phone, password, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(phone, password, **extra_fields)
+
+    def create_superuser(self, phone=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(phone, password, **extra_fields)
+
+
+class User(AbstractUser):
+    class Status(models.TextChoices):
+        PASSENGER = 'passenger','Passenger'
+        DRIVER = 'driver','Driver'
+        ADMIN = 'admin','Admin'
+    username = None
+    password = None
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+    phone = CharField(unique=True, max_length=20)
+    status = CharField(choices=Status.choices,  max_length=20)
+    language = CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = EmailField()
