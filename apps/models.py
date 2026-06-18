7b731df22712977110fcd630a8f7327e4d2c696e
muhammad023 <abdullaevmuhammad132@gmail.com>
2026-06-18 19:23:49 +0500

    db finish

--- apps/models.py
+++ apps/models.py
@@ -99,7 +99,7 @@ class Booking(AbstractUser):
     id = models.AutoField(primary_key=True)
     route = ForeignKey('apps.Route' , on_delete=CASCADE)
     passenger = ForeignKey('apps.User' , on_delete=CASCADE)
-    seats_number = models.SmallIntegerField(max_length=12)
+    seats_number = models.SmallIntegerField()
     status = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
