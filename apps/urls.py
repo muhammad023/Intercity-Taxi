aa30c5299de190b4f19e8299f9f6aaf028be5136
muhammad023 <abdullaevmuhammad132@gmail.com>
2026-06-19 20:30:04 +0500

    register start

--- apps/urls.py
+++ apps/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('register' , RegisterView.as_view(), name='register'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, CreateView, ListView, FormView, DetailView, DeleteView
+
 
 # Create your views here.
+
+class RegisterView(TemplateView):
+    template_name = 'register.html'
+
